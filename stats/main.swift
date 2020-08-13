7866a122ed1f78314a410d13cb048a2b6579e2a5
Hector De Diego <h.dediego@distillery.com>
2020-08-13 18:30:42 -0500

    feat: base support for status

--- stats/main.swift
+++ stats/main.swift
@@ -8,5 +8,64 @@
 
 import Foundation
 
-print("Hello, World!")
+/// Returns this keys line by line, each line defined as JSON [
+/// [
+///   "battery",
+///   "boottime",
+///   "cpuUsage",
+///   "diskUsage",
+///   "fans",
+///   "memoryUsage",
+///   "network",
+///   "processinfo",
+///   "uptime"
+/// ]
 
+let batteryStatus = getBattery()
+print("""
+"battery":\(batteryStatus)
+""")
+
+let bootTime: String = (try? getFormattedBootTimeDate()) ?? ""
+print("""
+"boottime":"\(bootTime)"
+""")
+
+let cpuUsage = getCPUUsage()
+print("""
+"cpuUsage":\(cpuUsage)
+""")
+
+let diskUsage = getDiskUsage()
+print("""
+"diskUsage":[]
+""")
+
+let fans = getFans()
+print("""
+"fans": "\(fans)"
+""")
+
+/// memoryUsage
+let memoryUsage = ""
+print("""
+"memoryUsage": "\(memoryUsage)"
+""")
+
+/// network
+let network = ""
+print("""
+"network": "\(network)"
+""")
+
+/// processinfo
+let processinfo = ""
+print("""
+"processinfo": "\(processinfo)"
+""")
+
+/// uptime
+let uptime = ""
+print("""
+"uptime": "\(uptime)"
+""")
